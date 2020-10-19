a5f84b5b6e9b8753b1f44f9cfb08bc8ba23a2ac2
Abdulkadir Sharaf <abdul.itquest@gmail.com>
2020-10-19 18:17:15 +0530

    Integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 10.times do |post|
     Post.create!(
-        title: "My Post title #(post)",
+        title: "My Post title #{post}",
         body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
     )
 end
 
 5.times do |skill|
     Skill.create!(
-        title: "Rails #(skill)",
+        title: "Rails #{skill}",
         percent_utilized: 15
     )
 end
 
 9.times do |portfolio_item|
     Portfolio.create!(
-        title: "Portfolio title: #(portfolio_item)",
+        title: "Portfolio title: #{portfolio_item}",
         subtitle: "My great service",
         body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's ",
         main_image: "http://placehold.it/600x400",

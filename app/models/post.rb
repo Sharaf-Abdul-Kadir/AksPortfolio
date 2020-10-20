40ba5cb1f8ce891e4be9fedd5a0af274879cc97e
Abdulkadir Sharaf <abdul.itquest@gmail.com>
2020-10-20 20:55:03 +0530

    Updated status for enum

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    enum status: { draft: 0, published: 1 }
     extend FriendlyId
     friendly_id :title, use: %i(slugged history finders)
     def should_generate_new_friendly_id?

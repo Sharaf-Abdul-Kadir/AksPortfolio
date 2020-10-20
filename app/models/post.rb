8c0ee4aba453b76779e1ec4920b78d5f0a3c4769
Abdulkadir Sharaf <abdul.itquest@gmail.com>
2020-10-20 18:49:58 +0530

    Integrated friendly routes for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    extend FriendlyId
+    friendly_id :title, use: %i(slugged history finders)
+    def should_generate_new_friendly_id?
+        slug.blank? || title_changed?
+    end
 end
